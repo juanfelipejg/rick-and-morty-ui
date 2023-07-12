df5cbb86b3c6cb43f6fd1bcd776c54ae28244edf
Juan Felipe Jaramillo <felipe.jaramillo@booktofly.com.co>
2023-07-11 20:13:02 -0500

    Url azure service was added

---
+++ src/app/core/resources/url.resources.ts
@@ -0,0 +1,3 @@
+export const URL_RESOURCE = {
+  apiUrl: "https://rickandmortyapi.azurewebsites.net/api"
+}
--- src/app/core/services/characters.service.ts
+++ src/app/core/services/characters.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 import { Store } from '@ngrx/store';
 import { LoadCharactersSuccess } from 'src/app/characters/actions/character.actions';
+import { URL_RESOURCE } from '../resources/url.resources';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,6 @@ export class CharactersService {
   constructor(private httpClient: HttpClient, private store: Store) { }
 
   public getAll(): void {
-    this.httpClient.get("https://localhost:7293/api/characters").subscribe( (characters) => this.store.dispatch( LoadCharactersSuccess({characters})));    
+    this.httpClient.get(`${URL_RESOURCE.apiUrl}/characters`).subscribe( (characters) => this.store.dispatch( LoadCharactersSuccess({characters})));    
   }
 }
